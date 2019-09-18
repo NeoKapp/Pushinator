9af4b1c8951680f67a321a74f5e0acb62340e86b
NeoKapp <mahu.alexandru.gabriel@gmail.com>
2019-09-18 21:28:37 +0300

    Add echo server

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var Push = require('pushover-notifications')
-var fs = require('fs')
+const express = require('express')
+
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+const Push = require('pushover-notifications')
+const fs = require('fs')
 
 const content = fs.readFileSync('./credentials.json','utf8')
 const data = JSON.parse(content)
+
+const app = express()
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
 
 var p = new Push( {
   user:data.USER_TOKEN,
@@ -17,17 +30,28 @@ var p = new Push( {
  
 var msg = {
   // These values correspond to the parameters detailed on https://pushover.net/api
-  message: '',	// required
+  message: 's',	// required
   title: "",
   sound: 'magic',
   device: '',
   priority: 1
 }
- 
 
-p.send(msg, function(err, result){
-  if ( err ) {
-    throw err
-  }
-  console.log(result)
+app.get('/',  (req, res) => {
+  res.send("hello");
 })
+
+app.post('/', (req, res) => {
+  console.log(req.body.message)
+  msg.message = req.body.message
+  p.send(msg, function(err, result){
+    if ( err ) {
+      throw err
+    }
+    console.log(result)
+  })
+})
+
+app.listen(3001, async () => {
+  console.log('listening on port 3001');
+});
